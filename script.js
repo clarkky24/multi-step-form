36c101520d490024ee21ddd97b57fe070bec5ca6
Clark Steven Dela Cruz <clarkstevendelacruz@gmail.com>
2023-04-29 18:50:55 +0800

    added prev/next button to the script

--- script.js
+++ script.js
@@ -7,6 +7,17 @@ const phone = document.getElementById('phone');
 const nameError = document.getElementById('nameError');
 const phoneError = document.getElementById('phoneError');
 const emailError = document.getElementById('emailError');
+const back = document.querySelector('.go-back');
+const next = document.querySelector('.next');
+const step = document.querySelectorAll('.step');
+const stepNumber = document.querySelectorAll('.step-number');
+const stepOne = document.getElementById('step-1');
+const stepTwo = document.getElementById('step-2');
+const formStep = form.querySelectorAll('.form-step');
+
+let currentStep = 0;
+
+
 
 // Functions
 
@@ -39,33 +50,33 @@ const emailValidation = () => {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
   //if statement
-    if(emailValue === ''){
-        emailError.textContent = '';
-        email.style.borderColor = 'hsl(227, 12%, 86%)';
-    } else if(!emailRegex.test(emailValue)){
-        emailError.textContent = 'Please enter a valid name';
-        emailError.style.color = 'red';
-        emailError.style.marginBottom = '1em';
-        emailError.style.fontSize = '.7em';
-        emailError.style.textAlign = 'center';
-        email.style.marginBottom = '.3em';
-        email.style.borderColor = 'red';
-    } else {
-        emailError.textContent = '';
-        email.style.borderColor = 'hsl(227, 12%, 86%)';
-    }
+  if (emailValue === '') {
+    emailError.textContent = '';
+    email.style.borderColor = 'hsl(227, 12%, 86%)';
+  } else if (!emailRegex.test(emailValue)) {
+    emailError.textContent = 'Please enter a valid name';
+    emailError.style.color = 'red';
+    emailError.style.marginBottom = '1em';
+    emailError.style.fontSize = '.7em';
+    emailError.style.textAlign = 'center';
+    email.style.marginBottom = '.3em';
+    email.style.borderColor = 'red';
+  } else {
+    emailError.textContent = '';
+    email.style.borderColor = 'hsl(227, 12%, 86%)';
+  }
 };
 
 //validation functions Ph phone number 
 const phoneValidation = () => {
-const phoneValue = phone.value.trim();
-const phoneRegex = /^(\+?63|0)[1-9][0-9]{9}$/;
+  const phoneValue = phone.value.trim();
+  const phoneRegex = /^(\+?63|0)[1-9][0-9]{9}$/;
 
   //if statement
-  if(phoneValue === ''){
+  if (phoneValue === '') {
     phoneError.textContent = '';
     phone.style.borderColor = 'hsl(227, 12%, 86%)';
-} else if(!phoneRegex.test(phoneValue)){
+  } else if (!phoneRegex.test(phoneValue)) {
     phoneError.textContent = 'Please enter a valid name';
     phoneError.style.color = 'red';
     phoneError.style.marginBottom = '1em';
@@ -73,15 +84,52 @@ const phoneRegex = /^(\+?63|0)[1-9][0-9]{9}$/;
     phoneError.style.textAlign = 'center';
     phone.style.marginBottom = '.3em';
     phone.style.borderColor = 'red';
-} else {
+  } else {
     phoneError.textContent = '';
     phone.style.borderColor = 'hsl(227, 12%, 86%)';
-}
+  }
 
 };
 
+
+
+
+// next and previous buttons
+
+
+function nextStep() {
+  formStep[currentStep].classList.remove('active');
+  currentStep++;
+  formStep[currentStep].classList.add('active');
+  back.style.visibility = 'visible';
+
+}
+
+function prevStep() {
+  formStep[currentStep].classList.remove('active');
+  currentStep--;
+  formStep[currentStep].classList.add('active');
+
+  if (currentStep === 0) {
+    back.style.visibility = 'hidden'; // Hide the button if on the first step
+  } else {
+    back.style.visibility = 'visible'; // Show the button if not on the first step
+  }
+}
+
+
+
+
+// avtive number of steps
+
+function activeNumber() {
+
+}
 
 // Event Listeners
 name.addEventListener('input', nameValidation);
 email.addEventListener('input', emailValidation);
 phone.addEventListener('input', phoneValidation);
+next.addEventListener('click', nextStep);
+back.addEventListener('click', prevStep);
+
